84d94463e8ea6caeb85a53316884239e69f15a9d
Haikun Xu <hkxu@iattc.org>
2019-06-12 16:23:44 -0700

    several commits

---
+++ R/AreaCode.R
@@ -0,0 +1,21 @@
+
+#' @export
+
+AreaCode = function(Lat,Lon,Species){
+  Areas <- rep(1,length(Lat))
+  
+  if(Species=="BET") {
+    i2 <- which(Lat>-10&Lat<10&Lon>-150&Lon<(-110))
+    Areas[i2] <- 2
+    i3 <- which(Lat>-10&Lon>-110)
+    Areas[i3] <- 3
+    i4 <- which(Lat<(-10)&Lon<(-110))
+    Areas[i4] <- 4
+    i5 <- which(Lat<(-10)&Lon>-110)
+    Areas[i5] <- 5
+    i6 <- which(Lon>-90&Lat<(-15))
+    Areas[i6] <- 6
+  }
+  
+  return(Areas)
+}
