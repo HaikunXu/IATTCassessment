a8d71f8363286faf407c973dbc8e29bd6a54e0c1
Haikun Xu <hkxu@iattc.org>
2019-11-06 15:29:21 -0800

    update cpue_fit by allowing for time-varying CPUE CV

--- R/cpue_fit.R
+++ R/cpue_fit.R
@@ -10,15 +10,18 @@ cpue_fit <- function(Path, s) {
     cpue <- myreplist$cpue
     n_fleet <- length(unique(cpue$Fleet))
     # ggplot(data=cpue) + geom_violin(aes(x=factor(Fleet),y=Dev))
-    rmse <- cpue %>% group_by(Fleet) %>% summarise(RMSE = sqrt(mean(Dev^2)))
+    rmse <- cpue %>% group_by(Fleet) %>% summarise(RMSE = sqrt(mean((Dev/SE)^2)))
     
-    f <- ggplot(data = cpue) + geom_point(aes(x = Time, y = Dev, color = factor(Fleet))) + geom_text(aes(x = -Inf, 
-        y = -Inf, label = paste0("RMSE = ",round(RMSE, 2))), data = rmse, hjust = -0.5, vjust = -1) + geom_smooth(aes(x = Time, y = Dev, 
-        color = factor(Fleet), fill = factor(Fleet)), span = s, se = FALSE) + facet_wrap(~Fleet, nrow = n_fleet) + geom_hline(yintercept = 0) + theme_bw(12) + ylab("CPUE residuals")
+    f <- ggplot(data = cpue) +
+        # geom_errorbar(aes(x = Time, ymin = -SE, ymax = SE), color = "black", alpha= 0.5) +
+        geom_point(aes(x = Time, y = Dev/SE, color = factor(Fleet))) + geom_text(aes(x = -Inf, 
+        y = -Inf, label = paste0("RMSE = ",round(RMSE, 2))), data = rmse, hjust = -0.5, vjust = -1) + geom_smooth(aes(x = Time, y = Dev/SE, 
+        color = factor(Fleet), fill = factor(Fleet)), span = s, se = FALSE) + facet_wrap(~Fleet, nrow = n_fleet) + geom_hline(yintercept = 0) +
+        theme_bw(12) + ylab("CPUE residual / CPUE std error") + xlab("Year")
     
-    ggsave(f, file = paste0(Path, "CPUE_Fit1.png"), width = 6, height = 6)
+    ggsave(f, file = paste0(Path, "CPUE_Fit1.png"), width = 8, height = 6)
     
-    cpue <- cpue %>% mutate(Quarter = (Time - 1.5)%%4 + 1)
+    cpue <- cpue %>% mutate(Quarter = (Time - 1)%%4 + 1)
     
     f <- ggplot(data = cpue) + geom_boxplot(aes(x = factor(Fleet), y = Dev, color = factor(Quarter))) + geom_hline(yintercept = 0) + 
         theme_bw(12) + ylab("CPUE residuals")
