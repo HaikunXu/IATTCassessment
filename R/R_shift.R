a4dcce46ef31b2bedc412d1a4a696d12f6903ba0
Haikun Xu <hkxu@iattc.org>
2020-04-09 11:46:43 -0700

    add R_shift and NLL to the management table

--- R/R_shift.R
+++ R/R_shift.R
@@ -7,10 +7,12 @@
 R_shift <- function(Rep = Rep){
   
   R <- Rep$recruit
+  
   R_med <- R %>% filter(era == "Main") %>%
     mutate(Period=ifelse(Yr<77,"Early","Late")) %>%
-    group_by(Period) %>% summarise(med=median(pred_recr))
-  R_shift <- R_med$med[2]/R_med$med[1]
+    group_by(Period) %>% summarise(med=median(pred_recr),dev_med=median(dev))
   
-  return(R_shift)
+  R_shift <- c(R_med$med[2]/R_med$med[1],R_med$dev_med[2]/R_med$dev_med[1])
+  
+  return(R_shift[1])
 }
