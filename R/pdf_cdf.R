a3f6b7051d1f7b333844daa7a547a00cfd7d40ea
Haikun Xu <haikunxu1990@gmail.com>
2021-09-13 14:53:09 -0700

    add pdf_cdf to the library for the Kobe plot

---
+++ R/pdf_cdf.R
@@ -0,0 +1,24 @@
+#' Fishing mortality by age group
+#' 
+#' \code{pdf_cdf} This function computes the fishing mortality by age group for the stock assessment report 
+#' 
+#' @export
+
+pdf_cdf = function(x, DF, n_model) {
+  R_y <- DF
+  mean <- R_y$Est
+  sd <- R_y$Std
+  weight <- R_y$Weight
+  
+  for (m in 1:n_model) {
+    if(m == 1) pdf <- dnorm(x,mean=mean[m],sd=sd[m]) * weight[m]
+    else pdf <- pdf + dnorm(x,mean=mean[m],sd=sd[m]) * weight[m]
+  }
+  
+  for (m in 1:n_model) {
+    if(m == 1) cdf <- pnorm(x,mean=mean[m],sd=sd[m]) * weight[m]
+    else cdf <- cdf + pnorm(x,mean=mean[m],sd=sd[m]) * weight[m]
+  }
+  
+  return(list("cdf"=cdf,"pdf"=pdf))
+}
