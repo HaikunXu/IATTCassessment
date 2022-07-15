c0e6de2b367ff3006d14d5883bb22ceec7f423b8
Haikun Xu <haikunxu1990@gmail.com>
2022-07-15 10:33:30 -0700

    update new proposed LL area code for bigeye

--- R/area_code.R
+++ R/area_code.R
@@ -8,15 +8,15 @@ area_code = function(Lat, Lon, Species) {
     Areas <- rep(1, length(Lat))
     
     if (Species == "BET") {
-        i2 <- which(Lat > -10 & Lat < 10 & Lon > -150 & Lon < (-110))
+        i2 <- which(Lat > -5 & Lat < 10 & Lon < (-105))
         Areas[i2] <- 2
-        i3 <- which(Lat > -10 & Lon > -110)
+        i3 <- which(Lat < (-5) & Lat > -15 & Lon < (-105))
         Areas[i3] <- 3
-        i4 <- which(Lat < (-10) & Lon < (-110))
+        i4 <- which(Lon > -105 & Lat > -15)
         Areas[i4] <- 4
-        i5 <- which(Lat < (-10) & Lon > -110)
+        i5 <- which(Lat < (-15) & Lon < (-90))
         Areas[i5] <- 5
-        i6 <- which(Lon > -90 & Lat < (-15))
+        i6 <- which(Lat < (-15) & Lon > -90)
         Areas[i6] <- 6
     }
     
