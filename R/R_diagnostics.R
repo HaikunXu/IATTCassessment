ce8746a0901b7352652d36d0e384f6efa0be1b9a
Haikun Xu <haikunxu1990@gmail.com>
2022-09-13 21:47:30 -0700

    update xxx

--- R/R_diagnostics.R
+++ R/R_diagnostics.R
@@ -13,7 +13,7 @@ R_diagnostics <- function(Rep = Rep){
   R_shift <- R_med$med[2]/R_med$med[1]
   
   R0 <- exp(Rep$parameters$Value[which(Rep$parameters$Label=="SR_LN(R0)")])
-  R_mean <- mean(R$pred_recr[which(R$era=="Main")])
+  R_mean <- mean(R$pred_recr[which(R$era=="Main"&R$Yr>76)])
   R_ratio <- R_mean/R0
   
   return(c(R_shift,R_ratio))
