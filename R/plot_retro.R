ba78dc65ccc8e967e9f01dc2c82f690304686e7c
Haikun Xu <haikunxu1990@gmail.com>
2024-04-12 13:16:08 -0700

    improve plot_retro by adding figure name as an input

--- R/plot_retro.R
+++ R/plot_retro.R
@@ -4,53 +4,63 @@
 #' 
 #' @export
 
-plot_retro = function(SS_Dir, lyear, fyear, Save_Dir, title, xlim, ylim) {
-    # spawning biomass ratio
-    for (i in 1:length(lyear)) {
-        Myreplist = r4ss::SS_output(dir=SS_Dir[i],ncols=400,covar=F,forecast=FALSE,verbose = FALSE,printstats = FALSE)
-        if(i==1) {
-            SBR <- data.frame("Year" = 1975 + (Myreplist$timeseries$Yr-1)/4, "SBR" = Myreplist$timeseries$SpawnBio/Myreplist$timeseries$SpawnBio[1], "Assess_Year" = lyear[i])   
-        }
-        else {
-            SBR <- rbind(SBR,data.frame("Year" = 1975 + (Myreplist$timeseries$Yr-1)/4, "SBR" = Myreplist$timeseries$SpawnBio/Myreplist$timeseries$SpawnBio[1], "Assess_Year" = lyear[i]))
-        }
+plot_retro = function(SS_Dir, lyear, fyear, Save_Dir, title, figure_name = "Retro_SB", xlim, ylim) {
+  # spawning biomass ratio
+  for (i in 1:length(lyear)) {
+    Myreplist = r4ss::SS_output(
+      dir = SS_Dir[i],
+      covar = F,
+      forecast = FALSE,
+      verbose = FALSE,
+      printstats = FALSE
+    )
+    if (i == 1) {
+      SBR <-
+        data.frame(
+          "Year" = 1975 + (Myreplist$timeseries$Yr - 1) / 4,
+          "SBR" = Myreplist$timeseries$SpawnBio,
+          "Assess_Year" = lyear[i]
+        )
     }
-    
-    SBR <- SBR %>% mutate(label1 = ifelse(Year>=fyear&Year<(Assess_Year+1),1,0), label2 = ifelse(Year==Assess_Year+0.75,1,0))
-    SBR$Assess_Year <- as.factor(SBR$Assess_Year+1)
-    
-    f1 <- ggplot(data=SBR %>% filter(label1==1)) +
-        geom_line(aes(x=Year,y=SBR,color=Assess_Year)) +
-        geom_point(aes(x=Year,y=SBR,color=Assess_Year),data = SBR %>% filter(label2==1),size=3) +
-        coord_cartesian(ylim=ylim,xlim=xlim) +
-        labs(x = "", y = "", color = "Year") +
-        theme_bw(20) + ggtitle(title) + theme(plot.title = element_text(hjust = 0.5))
-        
-    
-    ggsave(f1, file = paste0(Save_Dir, "Retro_SBR.png"), width = 8, height = 6)
-    return(f1)
-    
+    else {
+      SBR <-
+        rbind(
+          SBR,
+          data.frame(
+            "Year" = 1975 + (Myreplist$timeseries$Yr - 1) / 4,
+            "SBR" = Myreplist$timeseries$SpawnBio,
+            "Assess_Year" = lyear[i]
+          )
+        )
     }
-    # 
-    # # Spawning biomass
-    # for (i in 1:length(lyear)) {
-    #     if(i==1) {
-    #         SBR <- data.frame("Year" = 1975 + (Myreplist$timeseries$Yr-1)/4, "SBR" = Myreplist$timeseries$SpawnBio, "Assess_Year" = lyear[i])   
-    #     }
-    #     else {
-    #         SBR <- rbind(SBR,data.frame("Year" = 1975 + (Myreplist$timeseries$Yr-1)/4, "SBR" = Myreplist$timeseries$SpawnBio, "Assess_Year" = lyear[i]))
-    #     }
-    # }
-    # 
-    # SBR <- SBR %>% mutate(label1 = ifelse(Year>=fyear&Year<(Assess_Year+1),1,0), label2 = ifelse(Year==Assess_Year+0.75,1,0))
-    # SBR$Assess_Year <- as.factor(SBR$Assess_Year+1)
-    # 
-    # f2 <- ggplot(data=SBR %>% filter(label1==1)) +
-    #     geom_line(aes(x=Year,y=SBR,color=Assess_Year)) +
-    #     geom_point(aes(x=Year,y=SBR,color=Assess_Year),data = SBR %>% filter(label2==1),size=3) +
-    #     # coord_cartesian(ylim=c(0.15,0.3)) +
-    #     labs(x = "", y = "") +
-    #     theme_bw(20) + ggtitle(title) + labs(fill = "Year")
-    # 
-    # ggsave(f2, file = paste0(Save_Dir, "Retro_SB.png"), width = 8, height = 6)
+  }
+  
+  SBR <-
+    SBR %>% mutate(
+      label1 = ifelse(Year >= fyear &
+                        Year < (Assess_Year + 1), 1, 0),
+      label2 = ifelse(Year == Assess_Year + 0.75, 1, 0)
+    )
+  SBR$Assess_Year <- as.factor(SBR$Assess_Year + 1)
+  
+  f1 <- ggplot(data = SBR %>% filter(label1 == 1)) +
+    geom_line(aes(x = Year, y = SBR, color = Assess_Year)) +
+    geom_point(
+      aes(x = Year, y = SBR, color = Assess_Year),
+      data = SBR %>% filter(label2 == 1),
+      size = 3
+    ) +
+    # coord_cartesian(ylim = ylim, xlim = xlim) +
+    labs(x = "", y = "", color = "Year") +
+    theme_bw(20) + ggtitle(title) +
+  theme(plot.title = element_text(hjust = 0.5))
+  
+  ggsave(
+    f1,
+    file = paste0(Save_Dir, figure_name, ".png"),
+    width = 8,
+    height = 6
+  )
+  return(f1)
     
+}
