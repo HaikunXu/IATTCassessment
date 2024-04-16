0dfd0ddf7c26bdee196ba6cdbb8ca8b2d4eb7ab8
Haikun Xu <haikunxu1990@gmail.com>
2024-04-16 16:02:06 -0700

    update the calculation of R shift to make it reasonable for steepness < 1

--- R/R_diagnostics.R
+++ R/R_diagnostics.R
@@ -7,13 +7,13 @@
 R_diagnostics <- function(Rep = Rep){
   
   R <- Rep$recruit
-  R_med <- R %>% filter(era=="Main") %>%
-    mutate(Period=ifelse(Yr<77,"Early","Late")) %>%
-    group_by(Period) %>% summarise(med=median(pred_recr))
-  R_shift <- R_med$med[2]/R_med$med[1]
+  R_mean <- R %>% filter(era=="Main") %>%
+    mutate(Period=ifelse(Yr<81,"Early","Late")) %>%
+    group_by(Period) %>% summarise(mean_devs=mean(dev))
+  R_shift <- exp(R_mean$mean_devs[2] - R_mean$mean_devs[1])
   
   R0 <- exp(Rep$parameters$Value[which(Rep$parameters$Label=="SR_LN(R0)")])
-  R_mean <- mean(R$pred_recr[which(R$era=="Main"&R$Yr>76)])
+  R_mean <- mean(R$pred_recr[which(R$era=="Main")])
   R_ratio <- R_mean/R0
   
   return(c(R_shift,R_ratio))
