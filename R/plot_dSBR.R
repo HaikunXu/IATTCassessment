bfe5a3cdda44e07ae1555ad51478571b4ebdfaad
Haikun Xu <haikunxu1990@gmail.com>
2025-05-09 11:00:41 -0700

    add plotting dynamic SBR using approximated std

---
+++ R/plot_dSBR.R
@@ -0,0 +1,64 @@
+#' Plot quarterly SBR
+#' 
+#' \code{plot_dSBR} This function plots quarterly SBR for the stock assessment report 
+#' 
+#' @export
+
+plot_dSBR = function(SS_Dir, lyear, fyear, legend, Save_Dir, ymax, figure_name = "", title = "", xlim, alpha = 0.1, ylabel = "", xlabel = "") {
+  for (i in 1:length(lyear)) {
+    # read SB
+    cor_mat <- read.table(paste0(SS_Dir[i], "ss3.std"), skip = 1, fill = NA, header = FALSE)
+    names(cor_mat) <- c("index","name","value","std.dev")
+    SB_est <- cor_mat$value[which(cor_mat$name == "SSB_std")[3:((lyear[i] - (fyear[i]-1)) * 4 + 2)]]
+    SB_std <- cor_mat$std.dev[which(cor_mat$name == "SSB_std")[3:((lyear[i] - (fyear[i]-1)) * 4 + 2)]]
+    SBR_est <- cor_mat$value[which(cor_mat$name == "depletion")[1:((lyear[i] - (fyear[i]-1)) * 4)]]
+    SBR_std <- cor_mat$std.dev[which(cor_mat$name == "depletion")[1:((lyear[i] - (fyear[i]-1)) * 4)]]
+    
+    # read SB0
+    RepName <- paste0(SS_Dir[i], "Report.sso")
+    RepStart <- grep("Spawning_Biomass_Report_1 No_fishery_for_Z=M_and_dynamic_Bzero", readLines(RepName))
+    RepStart <- RepStart+2
+    RepEnd <- grep("NUMBERS_AT_AGE_Annual_1 No_fishery_for_Z=M_and_dynamic_Bzero", readLines(RepName))
+    RepEnd <- RepEnd-2
+    #RepDat<-readr::read_table2(RepName,col_names=FALSE,skip=RepStart,
+    #                          n_max = (RepEnd - RepStart),skip_empty_rows=FALSE)
+    RepDat<-read.table(RepName,header=FALSE,skip=RepStart,
+                       nrows = (RepEnd - RepStart),blank.lines.skip=FALSE)
+    dSB_est <- RepDat$V3[3:((lyear[i] - (fyear[i]-1)) * 4 + 2)]
+    # assume same CV for equalibrium and dynamic depletion
+    SB <- data.frame(est = SB_est / dSB_est,
+                     std = SBR_std / SBR_est * (SB_est / dSB_est),
+                     year = rep(fyear[i]:lyear[i], each = 4),
+                     yq = seq(fyear[i], lyear[i] + 0.75, 0.25))
+    
+    if(i==1) SB_A <- SB %>% mutate(Spec=legend[i])
+    else SB_A <- rbind(SB_A,SB %>% mutate(Spec=legend[i]))
+}
+
+SB_A <- SB_A %>% data.frame()
+
+f <- ggplot(data = SB_A) +
+  geom_ribbon(aes(
+    x = yq,
+    ymin = est - 1.96 * std,
+    ymax = est + 1.96 * std,
+    fill = Spec
+  ),
+  alpha = alpha) +
+  geom_line(aes(x = yq, y = est, color = Spec), size = 1) +
+  geom_point(aes(x = yq, y = est, color = Spec),
+             size = 2,
+             data = SB_A %>% filter(yq == year)) +
+  theme_bw(20) + xlab("") + ylab("") +
+  coord_cartesian(ylim = c(0, ymax),
+                  xlim = xlim,
+                  expand = FALSE) +
+  ggtitle(title) +
+  theme(plot.title = element_text(hjust = 0.5))
+
+ggsave(f, file = paste0(Save_Dir, figure_name, "-SB.png"), width = 12, height = 8)
+ggsave(f, file = paste0(Save_Dir, figure_name, "-SB.pdf"), width = 12, height = 8)
+
+return(SB_A)
+    
+}
