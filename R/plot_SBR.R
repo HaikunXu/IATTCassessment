5d3daff3bf73742514b0b408fb6855bdc8822ffd
Haikun Xu <haikunxu1990@gmail.com>
2025-05-09 11:01:09 -0700

    extend the data extraction to the first forecast year

--- R/plot_SBR.R
+++ R/plot_SBR.R
@@ -8,11 +8,13 @@ plot_SBR = function(SS_Dir, lyear, fyear, legend, Save_Dir, ymax, figure_name = 
     for (i in 1:length(lyear)) {
         cor_mat <- read.table(paste0(SS_Dir[i], "ss3.std"), skip = 1, fill = NA, header = FALSE)
         names(cor_mat) <- c("index","name","value","std.dev")
-        SB_est <- cor_mat$value[which(cor_mat$name == "depletion")[1:((lyear[i] - (fyear[i]-1)) * 4)]]
-        SB_std <- cor_mat$std.dev[which(cor_mat$name == "depletion")[1:((lyear[i] - (fyear[i]-1)) * 4)]]
+        SB_est <- cor_mat$value[which(cor_mat$name == "depletion")[1:((lyear[i] - (fyear[i]-1)) * 4 + 1)]]
+        SB_std <- cor_mat$std.dev[which(cor_mat$name == "depletion")[1:((lyear[i] - (fyear[i]-1)) * 4 + 1)]]
         # if(sum(SB_std)==0) SB_std <- cor_mat$std_dev[which(cor_mat$name == "depletion")[1:((lyear[i] - (fyear[i]-1)) * 4)]]
-        SB <- data.frame(est = SB_est, std = SB_std, year = rep(fyear[i]:lyear[i], each = 4), yq = seq(fyear[i], lyear[i] + 0.75, 0.25))
-    
+        SB <- data.frame(est = SB_est, std = SB_std, 
+                         year = c(rep(fyear[i]:lyear[i], each = 4), lyear[i] + 1),
+                         yq = seq(fyear[i], lyear[i] + 1, 0.25))
+        
         if(i==1) SB_A <- SB %>% mutate(Spec=legend[i])
         else SB_A <- rbind(SB_A,SB %>% mutate(Spec=legend[i]))
     }
